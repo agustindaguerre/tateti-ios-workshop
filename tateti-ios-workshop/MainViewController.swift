6f0e816c1964e346c9ac6f44f1961aba5a4ad1b7
Agustin Daguerre <agustin.daguerre@moove-it.com>
2017-05-29 16:00:44 -0300

    Menu with navigations

---
+++ tateti-ios-workshop/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  tateti-ios-workshop
+//
+//  Created by Agustin Daguerre on 5/29/17.
+//  Copyright © 2017 agustindaguerre. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    
+    @IBAction func newGameClick(_ sender: UIButton) {
+        performSegue(withIdentifier: "gameSegue", sender: self)
+    }
+    
+}
