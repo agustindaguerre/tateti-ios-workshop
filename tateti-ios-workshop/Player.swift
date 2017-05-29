1e3f89265c0fc41c02256d832210f27cc4f45d7e
Agustin Daguerre <agustindaguerre@users.noreply.github.com>
2017-05-29 09:47:05 -0300

    Basic TicTacToe View & Core data models (#1)
    * View with basic tic tac toe board and constraints
    
    * Game and Player models
    
    * CoreData WIP
    
    * Add Podfile
    
    * Remove Player class

---
+++ tateti-ios-workshop/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  tateti-ios-workshop
+//
+//  Created by Agustin Daguerre on 5/29/17.
+//  Copyright © 2017 agustindaguerre. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Player: NSManagedObject {
+}
