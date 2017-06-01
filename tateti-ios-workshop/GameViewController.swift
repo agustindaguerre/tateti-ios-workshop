f4c7dc5d948114951dd002bcb5e35392c4b387cf
Agustin Daguerre <agustin.daguerre@moove-it.com>
2017-06-01 15:44:55 -0300

    Game logic FULL

--- tateti-ios-workshop/GameViewController.swift
+++ tateti-ios-workshop/GameViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import SwiftMessages
 
 class GameViewController: UIViewController {
     
     var players: [Player] = []
     var playerOneTurn = true
     var ended = false
+    var clickedSpaces: [Int] = []
     
     private let presenter = GamePresenter(appDelegateParam: UIApplication.shared.delegate as? AppDelegate)
     
@@ -25,21 +27,43 @@ class GameViewController: UIViewController {
     
     
     @IBAction func buttonClick(_ sender: UIButton) {
-        if (ended) {
+        if (clickedSpaces.contains(sender.tag) || ended) {
             return
         }
-        
+        clickedSpaces.append(sender.tag)
         let buttonSymbol = playerOneTurn ? "X" : "O"
         sender.setTitle(buttonSymbol, for: UIControlState.normal)
         playerOneTurn = !playerOneTurn
         
         ended = presenter.movement(position: sender.tag)
         if (ended) {
+            let view = MessageView.viewFromNib(layout: .CardView)
+            var config = SwiftMessages.Config()
+            config.dimMode = .gray(interactive: true)
+            view.button = nil
+            
+            // Add a drop shadow.
+            view.configureDropShadow()
+            
             if let winner = presenter.getWinner() {
                 // Show winner message
+                view.configureTheme(.success)
+                let iconText = "🎉"
+                view.configureContent(title: "Winner!", body: "\(winner.name!) wins!", iconText: iconText)
             } else {
                 // If no winner, game tied
                 // Show tied message
+                view.configureTheme(.warning)
+                let iconText = "😴"
+                view.configureContent(title: "Boring!", body: "Game ends in a tie", iconText: iconText)
+            }
+            
+            // Show the message.
+            SwiftMessages.show(config: config, view: view)
+                
+                // Specify one or more event listeners to respond to show and hide events.
+            config.eventListeners.append() { event in
+                if case .didHide = event { self.navigationController?.popViewController(animated: true) }
             }
         }
     }
